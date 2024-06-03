ab603bc952228194327b60682681186d6ed9225d
Lucija Vuletić <lucijalasic5@gmail.com>
2024-06-03 14:57:02 +0200

    Implemented login form, styled login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React, { useState, useEffect, createContext } from 'react';
-import logo from './logo.svg';
+import React, { useState, createContext } from 'react';
 import './App.css';
-import InputField from './components/InputField/InputField';
 import LoginForm from './components/LoginForm/LoginForm';
 import { LoginContext } from './context/Context';
 import RegisterForm from './components/RegisterForm/RegisterForm';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const loginContext = createContext(LoginContext);
 
   return !isLoggedIn ? (
     <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
-      <RegisterForm />
+      <LoginForm />
     </LoginContext.Provider>
   ) : (
     <div>
-      <p>neka game forma</p>
+      <p>Game</p>
     </div>
   );
 }
