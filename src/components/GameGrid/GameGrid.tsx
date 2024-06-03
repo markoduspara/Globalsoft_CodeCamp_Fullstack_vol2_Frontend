4f8875488af8e998594341f11b74ee92e8d079dd
Marko Duspara <m.duspara@globalsoft.ba>
2024-06-03 17:19:41 +0200

    game grid

--- src/components/GameGrid/GameGrid.tsx
+++ src/components/GameGrid/GameGrid.tsx
@@ -1,41 +1,34 @@
-
 function Square() {
-    
-    return(
-        <>
-            <button></button>
-        </>
-    )
-
+  return (
+    <>
+      <button></button>
+    </>
+  );
 }
 
-function Row(content : any) {
-    return(
-        <div>
-            {content}
-        </div>
-    )
+function Row(content: any) {
+  return <div>{content}</div>;
 }
-
-let gridCount : number = Number(prompt("Grid Size?"));
+const gridCount = 3;
+//let gridCount : number = Number(prompt("Grid Size?"));
 function GameGrid() {
-    let RowSquares = Array.from({length: gridCount}, () => <Square />)
-    let Rows = Array.from({length:gridCount},()=> <Row content={RowSquares} />)
-    
-    console.log(Rows);
-    return (
+  let RowSquares = Array.from({ length: gridCount }, () => <Square />);
+  let Rows = Array.from({ length: gridCount }, () => (
+    <Row content={RowSquares} />
+  ));
+
+  console.log(Rows);
+  return (
+    <div>
+      {Rows.map(() => (
         <div>
-            {
-                Rows.map(()=> 
-                    <div>
-                        {RowSquares.map(()=> <Square/>)}
-                    </div>
-                )
-            }
+          {RowSquares.map(() => (
+            <Square />
+          ))}
         </div>
-
-    )
-
+      ))}
+    </div>
+  );
 }
 
 export default GameGrid;
